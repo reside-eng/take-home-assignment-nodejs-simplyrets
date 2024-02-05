9cd9b8905cbd21f502ca8c0fd65001a8cac43a98
Scott Prue <scott@sideinc.com>
2024-02-05 12:58:18 -0700

    chore(deps): switch to tsx over ts-node

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import { propertyRoutes } from './routes';
 
 const app = express();
+app.use('/', (req, res) => {
+  res.send('Some third message')
+});
 app.use('/properties', propertyRoutes);
 
 export default app;
