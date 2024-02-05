a3f94fd12c7b83bb79f1a695504b0538f56e6668
Scott Prue <scott@sideinc.com>
2024-02-05 12:59:24 -0700

    fix: cleanup message

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import express from 'express';
 import { propertyRoutes } from './routes';
 
 const app = express();
+// Index route
 app.use('/', (req, res) => {
-  res.send('Some third message')
+  res.send('Visit "/properties" to load properties')
 });
+
+// Properties route
 app.use('/properties', propertyRoutes);
 
 export default app;
